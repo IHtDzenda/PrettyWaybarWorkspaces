737ff57aa16ad821026c9f9d4266bea12695109c
IhtDzenda <nev27im@gmail.com>
2023-03-21 17:20:01 +0100

    added multi monitor support

--- workspaces.py
+++ workspaces.py
@@ -1,6 +1,7 @@
 import subprocess
 import json
 import os.path
+import argparse
 iconsNumeral = ["①","②","③","④","⑤","⑥","⑦","⑧","⑨","⑩","⑪ ", "⑫","⑬", "⑭","⑮ ","⑯", "⑰ ","⑱", "⑲"  ,"⑳" ]
 icons=""
 prefixStart="𓉘"
@@ -8,7 +9,11 @@ prefixEnd=" 𓉝"
 prefixActiveStart="﹃"
 prefixActiveEnd=" ﹄"
 usrdir=os.path.expanduser( '~' )
-
+#monitors
+parser = argparse.ArgumentParser()
+parser.add_argument('--m', type=str, default='eDP-1', help='use -m for monitor to get monitors use hyprctl -j  workspaces  | jq \'.[] | .monitor\'')
+args = parser.parse_args()
+display=args.m
 
 # Run hyprctl command and get output as JSON
 result = subprocess.run(["hyprctl", "-j", "workspaces"], stdout=subprocess.PIPE)
@@ -18,6 +23,7 @@ activeWorspaceID = monitor[0]["activeWorkspace"]["id"]
 json_data = json.loads(result.stdout)
 json_data = sorted(json_data, key=lambda x: x['id'])
 
+
 with open(usrdir+'/.config/waybar/scripts/linkedIcons.json', 'r') as f:
     linkedIcons = json.load(f)
 
@@ -25,23 +31,25 @@ for workspace in json_data:
     # Extract the id and lastwindowtitle fields from the object
     workspace_id = workspace["id"]
     title = workspace["lastwindowtitle"]
-    notDone=True
-    for item in linkedIcons:
-        if item['appName']in title:
-            if activeWorspaceID!=workspace_id:
-                icons=icons+prefixStart+item['icon']+prefixEnd
-                notDone =False
-            else:
-                icons=icons+prefixActiveStart+item['icon']+prefixActiveEnd
+    currentmonitor =workspace["monitor"]
+    if currentmonitor==display:
+        notDone=True
+        for item in linkedIcons:
+            if item['appName']in title:
+                if activeWorspaceID!=workspace_id:
+                    icons=icons+prefixStart+item['icon']+prefixEnd
+                    notDone =False
+                else:
+                    icons=icons+prefixActiveStart+item['icon']+prefixActiveEnd
 
-                notDone=False
-    
-    if notDone==True:
-        if activeWorspaceID!=workspace_id:
-            icons=icons+prefixStart+iconsNumeral[workspace_id-1]+prefixEnd
-        else:
-            icons=icons+prefixActiveStart+iconsNumeral[workspace_id-1]+prefixActiveEnd
-      
+                    notDone=False
+        
+        if notDone==True:
+            if activeWorspaceID!=workspace_id:
+                icons=icons+prefixStart+iconsNumeral[workspace_id-1]+prefixEnd
+            else:
+                icons=icons+prefixActiveStart+iconsNumeral[workspace_id-1]+prefixActiveEnd
+        
 
 
 print(icons)
